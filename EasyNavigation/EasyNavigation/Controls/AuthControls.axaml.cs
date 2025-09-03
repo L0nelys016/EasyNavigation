247335a3d720e0e945acfc828d2bbca1b0ba26f0
Artem Volkov <artemka.volkov2017@yandex.ru>
2025-09-03 11:26:27 +0300

    add paswword hiding

--- EasyNavigation/EasyNavigation/Controls/AuthControls.axaml.cs
+++ EasyNavigation/EasyNavigation/Controls/AuthControls.axaml.cs
@@ -1,11 +1,64 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 
 namespace EasyNavigation.Controls;
 
 public partial class AuthControls : UserControl
 {
+    private bool _isPasswordVisible = false;
+    private bool _regIsPasswordVisible = false;
+
     public AuthControls()
     {
         InitializeComponent();
     }
+
+    private void TogglePasswordVisibility(object sender, RoutedEventArgs e)
+    {
+        _isPasswordVisible = !_isPasswordVisible;
+
+        if (PasswordTextBox != null)
+            PasswordTextBox.PasswordChar = _isPasswordVisible ? '\0' : '*';
+
+        if (ShowPasswordButton != null && HidePasswordButton != null)
+        {
+            ShowPasswordButton.IsVisible = _isPasswordVisible;
+            HidePasswordButton.IsVisible = !_isPasswordVisible;
+        }
+    }
+
+    private void RegTogglePasswordVisibility(object sender, RoutedEventArgs e)
+    {
+        _regIsPasswordVisible = !_regIsPasswordVisible;
+
+        if (RegPasswordTextBox != null)
+            RegPasswordTextBox.PasswordChar = _regIsPasswordVisible ? '\0' : '*';
+
+        if (RegShowPasswordButton != null && RegHidePasswordButton != null)
+        {
+            RegShowPasswordButton.IsVisible = _regIsPasswordVisible;
+            RegHidePasswordButton.IsVisible = !_regIsPasswordVisible;
+        }
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+
+        if(ShowPasswordButton != null && HidePasswordButton != null)
+        {
+            ShowPasswordButton.IsVisible = false;
+            HidePasswordButton.IsVisible = true;
+        }
+
+        if(RegShowPasswordButton != null && RegHidePasswordButton != null)
+        {
+            RegShowPasswordButton.IsVisible = false;
+            RegHidePasswordButton.IsVisible = true;
+        }
+    }
+
 }
